dda6c6927cfbc99504dfe558dcfb668abdd1269f
CSY271 <59222287+SYChai271@users.noreply.github.com>
2022-07-07 00:17:56 +0800

    idk honestly

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Get the modal
 var modal = document.getElementById("id01");
+var allLoginsByClass = document.getElementsByClassName("login");
+var allProfilesByClass = document.getElementsByClassName("profile");
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
@@ -14,11 +16,19 @@ function onLogin() {
   } else if (window.location.pathname == "/index.html") {
     document.getElementById("about").style.display = "block";
   }
-  document.getElementById("login").style.display = "none";
-  document.getElementById("profile").style.display = "block";
+  for (var i of allLoginsByClass) {
+    i.style.display = "none";
+  }
+  for (var i of allProfilesByClass) {
+    i.style.display = "block";
+  }
   modal.style.display = "none";
 }
 
-document.getElementById("login").onclick = function () {
-  modal.style.display = "block";
-};
+window.onclick = function (event) {
+    for (var i of allLoginsByClass) {
+        if (event.target == i) {
+            modal.style.display = 'block';
+        }
+    }
+}
